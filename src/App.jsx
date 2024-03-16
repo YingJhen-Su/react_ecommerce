9a862504404047cc10dc78196eb1233d6668c4ce
YingJhen-Su <clearsky0330@gmail.com>
2024-03-17 00:12:28 +0800

    feat: modify project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
 
-function App() {
-    const [count, setCount] = useState(0);
+// layout
+import RootLayout from "./layouts/RootLayout";
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
-    );
-}
+// pages
+import Home from "./pages/home/Home";
+import Products from "./pages/product/Products";
+import ProductItem from "./pages/product/ProductItem";
+import CategoryItem from "./pages/category/CategoryItem";
+import Cart from "./pages/cart/Cart";
+import NotFound from "./pages/error/NotFound";
+
+// loader
+import {
+    categoryLoadder,
+    latestLoader,
+    productsLoader,
+} from "./helpers/loaders";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} loader={latestLoader} />
+            <Route path="products">
+                <Route index element={<Products />} loader={productsLoader} />
+                <Route path=":id" element={<ProductItem />} />
+            </Route>
+            <Route path="category">
+                <Route index element={<NotFound />} />
+                <Route
+                    path=":id"
+                    element={<CategoryItem />}
+                    loader={categoryLoadder}
+                />
+            </Route>
+            <Route path="cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+        </Route>
+    )
+);
+const App = () => {
+    return <RouterProvider router={router} />;
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./scss/index.scss";
+
+// FontAwesome
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
